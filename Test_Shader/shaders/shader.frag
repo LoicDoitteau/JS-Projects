b5852ca33b0cd8de9a038ce1e9d3c33d1ed776ee
LoicDoitteau <loic.doitteau@viacesi.fr>
2020-01-22 00:36:42 +0100

    Added palette

--- Test_Shader/shaders/shader.frag
+++ Test_Shader/shaders/shader.frag
@@ -6,14 +6,16 @@ uniform float uTime;
 
 uniform int rows;
 uniform int cols;
-uniform vec3 c;
-uniform sampler2D text;
+uniform vec3 color;
+uniform sampler2D texture;
+uniform sampler2D palette;
+uniform int count;
 uniform bool show;
 
 vec3 square(vec2 position, vec3 color)
 {
-	vec2 bl = step(vec2(0.01), position);			// Bottom left
-	vec2 tr = step(vec2(0.01), 1.0 - position);		// Top right
+	vec2 bl = step(vec2(0.1), position);			// Bottom left
+	vec2 tr = step(vec2(0.1), 1.0 - position);		// Top right
 	return color * (bl.x * bl.y * tr.x * tr.y);		// Square with black stroke
 }
 
@@ -24,7 +26,15 @@ void main()
 	vec2 pos = fract(uv * grid);					// Multiply cells count
 	vec2 cell = floor(uv * grid) / grid;			// Current cell coord
 	cell.y = 1.0 - cell.y;							// Flip Y coord
-	vec3 clr = texture2D(text, cell).rgb * c;
+	vec3 clr = texture2D(texture, cell).rgb * color;
 	vec3 sqr = show ? square(pos, clr) : clr;
 	gl_FragColor = vec4(sqr, 1.0);
+
+	if(show)
+	{
+		vec2 brush = vec2(floor(uv.x * float(count)) / float(count), 0.0);
+		vec4 plt = texture2D(palette, brush);
+		float val = step(uv.y, 0.98);
+		gl_FragColor = val * gl_FragColor + (1.0 - val) * plt;
+	}
 }
--- Test_Shader/sketch.js
+++ Test_Shader/sketch.js
@@ -1,10 +1,47 @@
 const SIZE = 600;
 let RESOLUTION;
 
+let palette;
 let img;
 let shd;
 let colorPicker;
 let checkbox;
+
+function createPalette() {
+  const COLORS = [
+    color(0, 0, 0),
+    color(255, 55, 55),
+    color(255, 98, 40),
+    color(255, 212, 38),
+    color(100, 182, 20),
+    color(44, 128, 44),
+    color(43, 185, 221),
+    color(43, 154, 174),
+    color(61, 65, 141),
+    color(151, 77, 157),
+    color(103, 51, 31),
+    color(255, 255, 255),
+    color(236, 192, 243),
+    color(202, 115, 175),
+    color(129, 182, 196),
+    color(186, 204, 222),
+    color(152, 211, 173),
+    color(249, 235, 155),
+    color(245, 219, 190),
+    color(241, 176, 160),
+    color(254, 113, 116),
+    color(252, 118, 155),
+    color(255, 187, 202)
+  ];
+  const palette = createImage(COLORS.length, 1);
+  palette.loadPixels();
+  for (let x = 0; x < COLORS.length; x++) {
+    const color = COLORS[x];
+    palette.set(x, 0, color);
+  }
+  palette.updatePixels();
+  return palette;
+}
 
 function preload() {
   img = loadImage("images/cat.jpg");
@@ -16,11 +53,17 @@ function setup() {
 
   pixelDensity(1);
 
+  palette = createPalette();
   colorPicker = createColorPicker(color(255, 228, 202));
   resolutionSlider = createSlider(1, SIZE, 50, 1);
-  checkbox = createCheckbox('show grid', false);
+  checkbox = createCheckbox('infos', false);
 
   shader(shd);
+
+  shd.setUniform('texture', img);
+  shd.setUniform('palette', palette);
+  shd.setUniform('count', 22);
+
   noStroke();
 }
 
@@ -38,8 +81,7 @@ function draw() {
 
   shd.setUniform('rows', RESOLUTION);
   shd.setUniform('cols', RESOLUTION);
-  shd.setUniform('c', [r, g, b]);
-  shd.setUniform('text', img);
+  shd.setUniform('color', [r, g, b]);
   shd.setUniform('show', checkbox.checked());
 
   quad(-1, -1, 1, -1, 1, 1, -1, 1);
