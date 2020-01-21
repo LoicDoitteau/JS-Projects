cce5e1ce77e5c913b4eaa278a90af918dc263cdb
DOITTEAU Loïc <loic.doitteau@econocom.com>
2020-01-21 17:22:47 +0100

    Grid display

--- Test_Shader/shaders/shader.frag
+++ Test_Shader/shaders/shader.frag
@@ -1,9 +1,27 @@
 precision highp float;
-varying vec2 vPos;
 
+uniform vec2 u_resolution;
+uniform vec3 u_mouse;
+uniform float u_time;
+
+uniform int rows;
+uniform int cols;
 uniform vec3 c;
+uniform sampler2D text;
+
+float square(vec2 position, float size)
+{
+	vec2 bl = step(vec2(0.01), position);
+	vec2 tr = step(vec2(0.01), 1.0 - position);
+	return bl.x * bl.y * tr.x * tr.y;
+}
 
 void main() 
 {
-	gl_FragColor = vec4(c,1.0);
+	vec2 st = gl_FragCoord.st/u_resolution;
+	vec2 pos = fract(st * vec2(cols, -rows));
+	float sqr = square(pos, 0.5);
+	gl_FragColor = vec4(c * sqr, 1.0);
+	// gl_FragColor = texture2D(text, pos) * vec4(c, 1.0);
+	// gl_FragColor = vec4(st, 0.0, 1.0);
 }
--- Test_Shader/sketch.js
+++ Test_Shader/sketch.js
@@ -1,21 +1,23 @@
-const WIDTH = 200;
-const HEIGHT = 200;
-// const ROWS = 200;
-// const COLS = 200;
+const WIDTH = 600;
+const HEIGHT = 600;
+const ROWS = 10;
+const COLS = 10;
 
-// let img;
+let img;
 let shd;
 let colorPicker;
 
 function preload() {
-  // img = loadImage("images/cat.jpg");
+  img = loadImage("images/cat.jpg");
   shd = loadShader('shaders/shader.vert', 'shaders/shader.frag');
 }
 
 function setup() {
   createCanvas(WIDTH, HEIGHT, WEBGL);
-  colorPicker = createColorPicker(color(52, 52, 52));
+  colorPicker = createColorPicker(color(255, 228, 202));
   shader(shd);
+  shd.setUniform('rows', ROWS);
+  shd.setUniform('cols', COLS);
   noStroke();
 }
 
@@ -24,6 +26,10 @@ function draw() {
   const r = red(clr) / 255;
   const g = green(clr) / 255;
   const b = blue(clr) / 255;
+  shd.setUniform("u_resolution", [float(width), float(height)]);
+  shd.setUniform("u_mouse", [float(mouseX), float(mouseY)]);
+  shd.setUniform("u_time", millis() / 1000.0);
   shd.setUniform('c', [r, g, b]);
+  shd.setUniform('text', img);
   quad(-1, -1, 1, -1, 1, 1, -1, 1);
 }
